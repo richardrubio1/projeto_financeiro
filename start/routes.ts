5568d84da9e7074dc4b387eba2699410520d9651
Richard Rubio <richardrubio12@gmail.com>
2025-05-24 16:56:24 -0300

    Atualização

--- start/routes.ts
+++ start/routes.ts
@@ -6,8 +6,11 @@ router.post('/login', '#controllers/auth_controller.login')
 router.post('/logout', '#controllers/auth_controller.logout').use(middleware.auth({ guards: ['api'] }))
 router.get('/me', '#controllers/auth_controller.me').use(middleware.auth({ guards: ['api'] }))
 
+
 // 👤 Cadastro de usuários (se aplicável)
 router.resource('/users', '#controllers/users_controller').apiOnly()
+router.resource('/tipos', '#controllers/tipos_controller').apiOnly()
+
 
 // 📂 Recurso protegido: Categorias
 router.resource('/categorias', '#controllers/categorias_controller').apiOnly()
